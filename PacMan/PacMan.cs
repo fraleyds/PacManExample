5ad069d2e94e5378fd8b6190ece575099ea311c4
David Fraley <fraleyds@gmail.com>
2018-04-18 11:05:49 -0400

    contracts and pacman class made

---
+++ PacMan/PacMan.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PacMan
+{
+    public class PacMan
+    {
+        public int Lives { get; set; }
+        public int Points { get; set; }
+        public int GhostCount { get; set; }
+        public int GainedLives { get; set; }
+    }
+}
---
+++ PacMan/PacManService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PacMan
+{
+    public class PacManService : IPacManService
+    {
+        public string[] ReadFile()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CheckOutcome()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int AddLives()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int AddPoints()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
